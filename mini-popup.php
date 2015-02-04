4d5f03f6d2412d76a12120a3b768c3920223581f
aikizbysio <aikizbysio@go2.pl>
2015-02-04 18:36:46 +0100

    Stworzenie pluginu

---
+++ mini-popup.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Mini PopUp Plugin
+Plugin URI: http://eBiznes-Praktycznie.pl
+Description: Wtyczka służy do tworzenia Pop Upów  - wyskakujących okienek do zapisu na listę za pomoacą shortcodu
+Version: 1.0
+Author: Zbigniew Adamski
+Author URI: http://ebiznes-praktycznie.pl/
+*/
+
+
+
+?>
